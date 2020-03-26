9a34f941fe3add4bbc80dfd15ffd1026e1b5586a
chelseachel <cheecheese@outlook.com>
2020-03-27 06:17:22 +0800

    finish projects

--- src/views/projects/components/ProjectsScroll.vue
+++ src/views/projects/components/ProjectsScroll.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-import ProjectsScrollItem from './ProjectsScrollItem'
+import ProjectsScrollItem from './children/ProjectsScrollItem'
 import { mapState } from 'vuex'
 export default {
   name: 'ProjectsScroll',
